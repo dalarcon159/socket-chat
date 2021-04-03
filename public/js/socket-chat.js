640cc0a9da57364133b7f6ffe32080d45f3ad52b
dalarcon159@gmail.com <dalarcon159@gmail.com>
2021-04-02 23:25:00 -0500

    hola

---
+++ public/js/socket-chat.js
@@ -0,0 +1,55 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+
+if(!params.has('nombre') || !params.has('sala')){
+    window.location = 'index.html';
+    throw new Error('EL nombre y sala necesario');
+}
+
+var usuario ={
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+}
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat',usuario,function(resp){
+        console.log('usuarios conectado ',resp)
+    });
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Fernando',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+});
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+socket.on('listaPersona', function(personas) {
+
+    console.log('Servidor:', personas);
+
+});
+
+socket.on('mensajePrivado',function(mensaje){
+
+    console.log('mensaje privado',mensaje)
+});
